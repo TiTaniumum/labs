eb851f19e4fd98de8ed10f459bf02b184a2ba133
Titanium <yelemanov.temirlan@gmail.com>
2018-02-16 10:48:34 +0600

    a+b defence

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine(); // 5 6
+            string[] arr = s.Split();
+            int a = int.Parse(arr[0]);
+            int b = int.Parse(arr[1]);
+            Console.WriteLine(a + b);
+            Console.ReadKey();
+        }
+    }
+}
